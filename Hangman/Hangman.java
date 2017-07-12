32bbebdbbf73f71b94372335b85822cb01ce3c5d
RicSala <ricsala86@gmail.com>
2017-07-12 12:08:55 +0200

    Hangman: Añadida función para ver si una letra es acierto o fallo

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -1,6 +1,6 @@
 public class Hangman {
 
-  public static void main(string[] args) {
+  public static void main(String[] args) {
 
     Game game = new Game("CodeThen");
 
--- Hangman/game.java
+++ Hangman/game.java
@@ -1,8 +1,25 @@
 class Game {
   private String answer;
+  private String hits;
+  private String misses;
 
-  public Game(String Answer) {
+  public Game(String answer) {
     this.answer = answer; //así evitamos que colisionen las variables
+    hits = ""; //inicializa la variable hits a "" --> Acabamos de empezar aún no hay hits
+    misses = "";  //inicializa la variable misses a "" --> idem
+
   }
+
+  public boolean applyGuess(char letter) {
+    boolean isHit = ( answer.indexOf(letter) != -1 );
+    if (isHit) {
+      hits += letter;
+    }
+    else {
+      misses += letter;
+    }
+    return isHit;
+  }
+
 
 }
